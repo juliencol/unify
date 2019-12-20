c6543c34c5d7ce4aa2a0f3428e8dfd1557f70855
Julien Colombain <julien@colombain.com>
2019-12-20 17:31:25 +0100

    home page created with david

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,7 @@
 class EventsController < ApplicationController
     before_action :authenticate_user!
+
+    def index
+        @events = Event.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
-  root to: 'pages#home'
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords', confirmations: 'users/confirmations' }
+  root to: 'events#index'
+  get '/events', to: 'events#index', as: '/events'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
