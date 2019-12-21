87b6fae2cd9c87139dcf90aae73fa389a821b6ff
shinua1 <dylan_hu@hotmail.fr>
2019-12-21 17:43:19 +0100

    page EditProfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords', confirmations: 'users/confirmations' }
+  get 'users/edit'
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords', confirmations: 'users/confirmations',profile: 'users/profile', edit: 'users/edit' }
   root to: 'events#index'
   get '/events', to: 'events#index', as: '/events'
+  get '/users', to: 'users#show', as: '/profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
