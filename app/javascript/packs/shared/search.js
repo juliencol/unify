c470441a84a76c6ea8c4a1f6eb2fa3d53c764931
maejml <maelle.jumel@free.fr>
2019-12-29 00:09:37 +0100

    started backend searchbar

--- app/javascript/packs/shared/search.js
+++ app/javascript/packs/shared/search.js
@@ -1,7 +1,7 @@
-document.getElementById("search_terms").onfocus = function () {
+document.getElementById("search_name").onfocus = function () {
   document.getElementById("search-form").width = "40vw!important";
 };
 
-document.getElementById("search_terms").onblur = function () {
+document.getElementById("search_name").onblur = function () {
   document.getElementById("search-form").width = "25vw!important";
 };
