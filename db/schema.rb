be64343da1a6c34b51f08b3fd887424c1ed09976
Julien Colombain <julien@colombain.com>
2020-02-05 23:10:18 +0100

    adding theme column to events table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_160640) do
+ActiveRecord::Schema.define(version: 2020_02_05_220854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_160640) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "theme"
     t.index ["club_id"], name: "index_events_on_club_id"
   end
 
