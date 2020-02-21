b23818eb19aefe774898f5c84425013ed8de0065
Julien Colombain <julien@colombain.com>
2020-02-21 18:37:40 +0100

    correcting path in staging config file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,5 @@
 # Just use the production settings
-require File.expand_path('../production.rb', __FILE__)
+require File.expand_path('production.rb', __FILE__)
 
 Rails.application.configure do
   # Here override any defaults
