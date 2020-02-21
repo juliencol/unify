0a5ff69e5acb954c4eb85da037fff2308b144bb9
Julien Colombain <julien@colombain.com>
2020-02-21 18:42:59 +0100

    forgot the r of rails please don't judge me

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-ails.application.configure do
+Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
 
