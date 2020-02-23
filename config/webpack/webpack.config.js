7bbf3aa8062555400f6ede88a45002b9d0ed5f24
maejml <maelle.jumel@free.fr>
2020-02-23 12:40:32 +0000

    webpack config clean

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -30,17 +30,6 @@ module.exports = {
           }
         ]
       },
-      {
-        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: '../fonts/',
-            publicPath: '../static/fonts'
-          }
-        }]
-      },
     ]
   },
 };
