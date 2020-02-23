afa59139d7be73ac938ae1d8ebbda20eba0484de
maejml <maelle.jumel@free.fr>
2020-02-23 12:32:11 +0000

    font awesome fix #1

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -29,7 +29,18 @@ module.exports = {
             loader: 'font-awesome-loader'
           }
         ]
-      }
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: '../fonts/',
+            publicPath: '../static/fonts'
+          }
+        }]
+      },
     ]
   },
 };
