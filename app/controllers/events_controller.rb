c77992c36efce805ad55111d1c68fc7092fc8e43
Andrew Pouret <apouret@juniorisep.com>
2020-02-28 11:26:07 +0100

    added minimal logic for event update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,41 @@
 class EventsController < ApplicationController
-    def index
-        @events = Event.all
-        @themes = Theme.all
-        @clubs = Club.all
-        @search = params["search"]
-        if @search.present?
-          @name = @search["name"]
-          @events = Event.where("name ILIKE ?", "%#{@name}%")
-        end
+  def index
+    @events = Event.all
+    @themes = Theme.all
+    @clubs = Club.all
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      @events = Event.where("name ILIKE ?", "%#{@name}%")
     end
+  end
 
-    def show
-      @event = Event.find(params[:id])
-      @marker = {
-        lat: @event.latitude,
-        lng: @event.longitude
-      }
+  def show
+    @event = Event.find(params[:id])
+    @marker = {
+      lat: @event.latitude,
+      lng: @event.longitude,
+    }
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(events_params)
+      redirect_to @event
+    else
+      render "edit"
     end
+  end
+
+  private
+
+  def events_params
+    parms.require(:event).permit(:name, :short_description, :long_description,
+                                 :date, :image, :banner, :is_free, :latitude, :longitude)
+  end
 end
