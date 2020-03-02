9de64aeca6c3f2419e803beb9897f8ae8166259f
Julien Colombain <julien@colombain.com>
2020-03-02 18:13:14 +0100

    new seed

---
+++ db/seeds/shared/themes_data.rb
@@ -0,0 +1,13 @@
+THEMES_DATA = [
+    { title: "Conférence" },
+    { title: "Workshop" },
+    { title: "Afterwork" },
+    { title: "Soirée" },
+    { title: "Entrepreneuriat" },
+    { title: "Blockchain" },
+    { title: "Hackaton" },
+    { title: "Intelligence Artificielle" },
+    { title: "Dégustation" },
+    { title: "Maraude" },
+    { title: "Voyage" },
+]
