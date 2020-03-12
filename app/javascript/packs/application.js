77e99d985945a6e2cf72e20f085c59453a407f88
maejml <maelle.jumel@free.fr>
2020-03-12 20:15:10 +0000

    font awesome restore

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,7 @@ require('channels');
 
 require('jquery');
 require('bootstrap');
-require("@fortawesome/fontawesome-free/js/all");
+require("@fortawesome/fontawesome-free/css/all");
 require('../stylesheets/application');
 
 // Mapbox 
