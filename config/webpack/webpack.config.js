e4e1dc970506d3f83f5606f8a5c35b264bc7a087
maejml <maelle.jumel@free.fr>
2020-03-12 21:24:23 +0000

    build fix #4

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -45,15 +45,11 @@ module.exports = {
                 }
             },
             {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
                 use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts/'
-                    }
+                    loader: 'file-loader'
                 }]
-            }
+            },
         ]
     },
 };
