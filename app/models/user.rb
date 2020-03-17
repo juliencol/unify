b0517ed231d12054226bf0da8cd35e321208e642
Julien Colombain <julien@colombain.com>
2020-03-17 17:02:40 +0100

    new sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :email, :presence => true, format: { with: /\A(([A-Za-z0-9]*\.+*_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\+)|([A-Za-z0-9]+\+))*[A-Z‌​a-z0-9]+.+(([A-Za-z0-9]*\.+*_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\+)|([A-Za-z0-9]+\+))*[A-Z‌​a-z0-9]+@isep.fr/,
   message: "adresse isep uniquement" }
+  validates :first_name, :presence => { message: "Vous devez entrer votre prénom" }
+  validates :last_name, :presence => { message: "Vous devez entrer votre nom" }
+  validates :promotion, :presence => { message: "Vous devez entrer votre promotion" }
+  validates :section, :presence => { message: "Vous devez entrer votre section" }
   
   has_many :user_clubs
   has_many :clubs, through: :user_clubs
@@ -13,4 +17,4 @@ class User < ApplicationRecord
   has_many :events, through: :user_events
 
   mount_uploader :profile_picture, PhotoUploader
-end
+end 
