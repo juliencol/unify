2e86c447b3b6d03ac9fa94b97e1941d6addb7c89
Julien Colombain <julien@colombain.com>
2020-03-24 10:09:15 +0100

    added family banners

--- db/seeds/shared/families_data.rb
+++ db/seeds/shared/families_data.rb
@@ -1,26 +1,26 @@
 FAMILIES_DATA = [
     { name: "White Panda", 
-      banner: ""
+      banner: "https://res.cloudinary.com/isep/image/upload/v1585040696/unify/family-banners/Family-banner_panda_uanhfq.png"
     },
     { name: "Blue Lion", 
-      banner: ""
+      banner: "https://res.cloudinary.com/isep/image/upload/v1585040696/unify/family-banners/Family-banner_lion_wr9z2t.png"
     },
     { name: "Orange Tiger", 
-      banner: ""
+      banner: "https://res.cloudinary.com/isep/image/upload/v1585040697/unify/family-banners/Family-banner_tiger_ycest4.png"
     },
     { name: "Green Gorilla", 
-      banner: ""
+      banner: "https://res.cloudinary.com/isep/image/upload/v1585040696/unify/family-banners/Family-banner_gorilla_xqyiw6.png"
     },
     { name: "Red Fox", 
-      banner: ""
+      banner: "https://res.cloudinary.com/isep/image/upload/v1585040696/unify/family-banners/Family-banner_fox_lwdoez.png"
     },
     { name: "Yellow Eagle", 
-      banner: ""
+      banner: "https://res.cloudinary.com/isep/image/upload/v1585040696/unify/family-banners/Family-banner_eagle_unz1us.png"
     },
     { name: "Grey Bear", 
-      banner: ""
+      banner: "https://res.cloudinary.com/isep/image/upload/v1585040696/unify/family-banners/Family-banner_bear_grxmqt.png"
     },
     { name: "Black Rhino", 
-      banner: ""
+      banner: "https://res.cloudinary.com/isep/image/upload/v1585040697/unify/family-banners/Family-banner_rhino_rnrfnz.png"
     },  
 ]
