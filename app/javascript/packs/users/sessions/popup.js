84fc881b18610049b5ea98abaf41419fa73d371c
maejml <maelle.jumel@free.fr>
2020-03-30 10:56:21 +0200

    fixed all user pages

--- app/javascript/packs/users/sessions/popup.js
+++ app/javascript/packs/users/sessions/popup.js
@@ -1,7 +1,9 @@
-$(".pw-input").on("focus", function () {
-  document.querySelector('.forgot-password').classList.add('focused');
+$(".pw-input").on("focus", function() {
+    document.querySelector(".forgot-password").classList.add("focused");
 });
 
-/* $(".pw-input").on("blur", function () {
-  setTimeout(document.querySelector('.forgot-password').classList.remove('focused'), 1000);
-}); */
+$(".pw-input").on("blur", function() {
+    window.setTimeout(function() {
+        document.querySelector(".forgot-password").classList.remove("focused");
+    }, 500);
+});
