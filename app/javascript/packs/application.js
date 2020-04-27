7c76a9f138e896ebb587ca86e2ac7a0802cd4ba6
Julien Colombain <julien@colombain.com>
2020-04-27 10:50:25 +0200

    remove service worker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,11 +25,3 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 initMapbox();
 
-// Service worker for PWA
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
-    .then(function (reg) {
-      console.log('[Companion]', 'Service worker registered!');
-      console.log(reg);
-    });
-}
