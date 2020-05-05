97e322889c28a7a48d150e06da64ecffbd77305e
Julien Colombain <julien@colombain.com>
2020-05-05 14:55:10 +0200

    updating test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,25 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # emails
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.delivery_method = :smtp # :letter_opener
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.preview_path = "#{Rails.root}/rails/mailers"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.mailer_sender = "unifyisep@gmail.com"
+
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_APP_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
