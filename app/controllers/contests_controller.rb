9ae2b468b57b301b99eefaf49a64ed268cdc3b6a
Julien Colombain <julien@colombain.com>
2020-05-06 23:46:46 +0200

    Added a validation to answer all questions

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -15,18 +15,29 @@ class ContestsController < ApplicationController
   end
 
   def send_quizz
+    # Get the user's answers
     @contest = Contest.find(params[:contest_id])
     @questions = @contest.questions.includes([:answer_options])
     authorize @contest
     current_user.contests << @contest
-    user_contest = current_user.contests.select { |contest| contest.title == @contest.title }.first
-    user_contest.questions.each do |question|
+    @questions.each do |question|
       question[:user_answer] = params["#{question.id}"][:answer_options]
       question.save
     end
-    # TODO : make sure every questions were answered before submitting request
-    redirect_to contests_path
-    flash[:notice] = "Ta réponse a été prise en compte"
+
+    # Check every question was answered
+    number_of_answers = 0
+    @questions.each do |question|
+      number_of_answers += 1 if question.user_answer.present?
+    end
+    if number_of_answers != @questions.size
+      current_user.contests.delete(@contest)
+      flash[:notice] = "Il faut répondre à toutes les questions"
+      render "show"
+    else
+      redirect_to contests_path
+      flash[:notice] = "Ta réponse a été prise en compte"
+    end
   end
 
   def remove_winner
