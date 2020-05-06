cdbb4afb7679dfef91092831efb733bc5d35aec1
maejml <maelle.jumel@free.fr>
2020-05-06 21:56:43 +0200

    added link in show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_095235) do
+ActiveRecord::Schema.define(version: 2020_05_06_182450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_095235) do
     t.string "banner_image"
     t.string "short_description", limit: 60
     t.string "facebook_url"
+    t.string "discord_url"
   end
 
   create_table "companies", force: :cascade do |t|
