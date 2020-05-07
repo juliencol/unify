932196f79a8bb0f859f4e8a642fffe4b7f366981
Julien Colombain <julien@colombain.com>
2020-05-08 00:34:46 +0200

    trying again email confirmations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,19 +2,18 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # email settings
-  config.action_mailer.default_url_options = { host: "https://unifyisep.com" }
-  config.action_mailer.delivery_method = :smtp 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_caching = false
-  config.mailer_sender = "unifyisep@gmail.com"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'unifyisep.com' }
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_APP_PASSWORD'],
-    enable_starttls_auto: true
+    :port => 587,
+    :domain => "mail.google.com",
+    :user_name => ENV['GMAIL_ADDRESS'],
+    :password => ENV['GMAIL_APP_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
   # Code is not reloaded between requests.
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,19 +6,18 @@ Rails.application.configure do
   config.serve_static_files = true
 
   # email
-  config.action_mailer.default_url_options = { host: "https://unifyisep-staging.herokuapp.com" }
-  config.action_mailer.delivery_method = :smtp 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_caching = false
-  config.mailer_sender = "unifyisep@gmail.com"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'unifyisep-staging.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_APP_PASSWORD'],
-    enable_starttls_auto: true
+    :port => 587,
+    :domain => "mail.google.com",
+    :user_name => ENV['GMAIL_ADDRESS'],
+    :password => ENV['GMAIL_APP_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
 
