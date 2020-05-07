f006559700d7a43f5f2c9039930ee40057afb1a1
Julien Colombain <julien@colombain.com>
2020-05-08 00:24:15 +0200

    trying again email confirmations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,21 +2,18 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # email settings
-  config.action_mailer.default_url_options = { host: "https://www.unifyisep.com" }
-  config.action_mailer.delivery_method = :smtp # :letter_opener
-  config.action_mailer.smtp_settings = {:address => "www.unifyisep.com", :port => 25}
-  config.action_mailer.preview_path = "#{Rails.root}/rails/mailers"
+  config.action_mailer.default_url_options = { host: "https://unifyisep.com" }
+  config.action_mailer.delivery_method = :smtp 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.mailer_sender = "unifyisep@gmail.com"
-
-  ActionMailer::Base.smtp_settings = {
-    address: "smtp.gmail.com",
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
     port: 587,
-    domain: 'gmail.com',
+    domain: "gmail.com",
+    authentication: "plain",
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_APP_PASSWORD'],
-    authentication: :login,
     enable_starttls_auto: true
   }
 
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,21 +6,18 @@ Rails.application.configure do
   config.serve_static_files = true
 
   # email
-  config.action_mailer.default_url_options = { host: "https://unifyisep-staging.herokuapp.com/" }
-  config.action_mailer.delivery_method = :smtp # :letter_opener
-  config.action_mailer.smtp_settings = {:address => "unifyisep-staging.herokuapp.com/", :port => 25}
-  config.action_mailer.preview_path = "#{Rails.root}/rails/mailers"
+  config.action_mailer.default_url_options = { host: "https://unifyisep-staging.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.mailer_sender = "unifyisep@gmail.com"
-
-  ActionMailer::Base.smtp_settings = {
-    address: "smtp.gmail.com",
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
     port: 587,
-    domain: 'gmail.com',
+    domain: "gmail.com",
+    authentication: "plain",
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_APP_PASSWORD'],
-    authentication: :login,
     enable_starttls_auto: true
   }
 end
