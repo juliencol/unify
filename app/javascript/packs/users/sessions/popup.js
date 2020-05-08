e8b93e9413f36861572b0ca268f116c05625209f
maejml <maelle.jumel@free.fr>
2020-05-08 22:11:50 +0200

    corrected users pages

--- app/javascript/packs/users/sessions/popup.js
+++
@@ -1,9 +0,0 @@
-$(".pw-input").on("focus", function() {
-    document.querySelector(".forgot-password").classList.add("focused");
-});
-
-$(".pw-input").on("blur", function() {
-    window.setTimeout(function() {
-        document.querySelector(".forgot-password").classList.remove("focused");
-    }, 500);
-});
--- app/javascript/packs/users/users.js
+++ app/javascript/packs/users/users.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
+    function resizeForm() {
+        const button = document.querySelector(".form-actions"),
+            buttonSize = button.offsetWidth,
+            modalSize = document.querySelector(".login-container").offsetWidth,
+            buttonLeft = modalSize / 2 - buttonSize / 2;
+        button.style.left = buttonLeft + "px";
+    }
 
-  function resizeForm() {
-    const button = document.querySelector('.form-actions'),
-      buttonSize = button.offsetWidth,
-      modalSize = document.querySelector(".login-container").offsetWidth,
-      buttonLeft = modalSize / 2 - buttonSize / 2;
-    button.style.left = buttonLeft + "px";
-  }
-
-  window.onresize = resizeForm;
-  resizeForm();
+    window.onresize = resizeForm;
+    resizeForm();
 });
